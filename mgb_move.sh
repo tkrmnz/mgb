ddfef49db9c8bd39c819bc43cb0230c6f6f76f6c
John Yeh <tnkrmnz@gmail.com>
2014-09-11 18:15:01 -0400

    move batch

---
+++ mgb_move.sh
@@ -0,0 +1,17 @@
+cp update_chk.sh /root/update_chk.sh
+cp btunpair /root/btunpair
+cp btscan /root/btscan
+cp btslct /root/btslct
+cp btpair /root/btpair
+cp run_js_ev3.sh /root/run_js_ev3.sh
+cp run_js2.sh /root/run_js2.sh
+cp jt3 /root/jt3
+cp jt4 /root/jt4
+cp BTJSx.sh /root/BTJSx.sh
+cp BTJS3.sh /root/BTJS3.sh
+cp BTJSxd /etc/init.d/BTJSxd
+cp BTJS3d /etc/init.d/BTJS3d
+cd chhmod 755 /etc/init.d/BTJSxd
+cd chhmod 755 /etc/init.d/BTJS3d
+
+
